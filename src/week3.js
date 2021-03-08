8257affffd873350ca688fc1df0f855f391417d2
Holly499 <you@example.comholly-todd@nmit.ac.nz>
2021-03-09 10:56:48 +1300

    javascript practice

---
+++ src/week3.js
@@ -0,0 +1,27 @@
+console.log ('Holly')
+console.log ('testing testing 123')
+console.log ('test number 2')
+// todo javascript variables
+// note listen 
+// Declare a variable first name and assign a value string
+
+var firstName = 'Holly'
+// Print it
+console.log(firstName)
+
+var lastName = 'Todd'
+// Print it
+console.log(lastName)
+
+// let fullName = firstName + ' ' + lastName
+
+fullName = `My full name is ${firstName} ${lastName}` // ES6
+// Print it
+console.log (fullName)
+
+
+
+
+
+
+

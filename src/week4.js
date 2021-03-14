cf1161731bbdafb96e0673e489d9ad74aed36e56
Holly499 <holly-todd@live.nmit.ac.nz>
2021-03-15 12:49:07 +1300

    javascript variables and values

---
+++ src/week4.js
@@ -0,0 +1,23 @@
+//REVIEW
+/**
+ * Values and Variables
+ */
+
+let country = "New Zealand"
+let city = "Nelson"
+let postalCode = "7010"
+
+console.log(typeof country);
+console.log(typeof city);
+console.log(typeof postalCode);
+
+
+
+
+let isisland = true
+let language
+// this variable will be completed once data located
+
+console.log (typeof isisland)
+console.log (typeof language)
+

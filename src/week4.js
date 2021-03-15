e63ee1e299218a52ad00b1171f8bc922410055f3
Holly499 <holly-todd@live.nmit.ac.nz>
2021-03-16 10:35:17 +1300

    more work on JS

--- src/week4.js
+++ src/week4.js
@@ -3,13 +3,13 @@
  * Values and Variables
  */
 
-let country = "New Zealand"
-let city = "Nelson"
-let postalCode = "7010"
+const country = 'New Zealand'
+let city = 'Nelson'
+let postalCode = '7010'
 
-console.log(typeof country);
-console.log(typeof city);
-console.log(typeof postalCode);
+console.log(typeof country)
+console.log(typeof city)
+console.log(typeof postalCode)
 
 
 
@@ -20,4 +20,112 @@ let language
 
 console.log (typeof isisland)
 console.log (typeof language)
+
+language = 'english'
+
+let number = 10
+console.log (number)
+
+console.log(number / 2)
+console.log(number > 6)
+console.log(number < 2)
+console.log(number - 4)
+
+
+const address = country + " " + city + " " + postalCode
+console.log(address)
+
+let string= `my address is ${country}, ${city} and ${postalCode}.`
+
+console.log(string)
+
+10;
+
+let x = 10
+
+'Holly'
+
+if (x < 0) {
+console.log('Holly')
+} 
+else {
+    console.log('its false')
+}
+
+if (x < 140) {
+    console.log('lewis')
+}
+else {
+    console.log('its false')
+}
+
+if(x === 10) {
+    console.log('this is true')
+}
+
+if (typeof city === 'string') {
+    console.log('correct')
+
+} else { console.log('incorrect')
+}
+
+if(country === 'Australia') {
+    console.log('yes')
+} else { console.log('no')
+}
+//15/03/21 day end
+
+//16/03/21 day start
+
+let phoneNumber
+console.log(phoneNumber)
+
+phoneNumber = '035432454'
+console.log(phoneNumber)
+
+
+function name () {
+
+}
+
+x = '1234567'
+console.log(Number)
+
+
+
+
+const a = 1 + 1
+console.log(a)
+
+const b = a + 2
+console.log(b)
+
+console.log(a+b)
+
+
+const information = {
+    student: 'Holly',
+    country: 'New Zealand',
+    city: 'Motueka',
+    postcode: 7120,
+}
+    
+    console.log(information)
+    
+    let c = 11 
+
+    if(c === 11 || typeof c === 'number') {
+        console.log('its true')
+    } else {
+        console.log('error')
+    }
+
+    console.log(typeof c)
+
+
+   
+
+
+
+
 
